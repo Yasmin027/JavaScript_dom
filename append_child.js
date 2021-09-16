de77eeb2e4f823b8ea5e9cffa0b554b3c0f4fef2
Yasuko <89954618+Yasmin027@users.noreply.github.com>
2021-09-16 16:01:37 +0900

    Update append_child.js

--- append_child.js
+++ append_child.js
@@ -1,3 +1,45 @@
+
+
+【DOMツリーの階層関係から要素を取得する】
+
+eg.
+<ul>
+  <li>item</li>
+  <li>item</li>
+  <li>item</li>
+</ul>
+※要素Node以外にも空白や改行を表すテキストNodeもある
+
+ulの子Nodeを取得したいとき
+→ul.childNode （改行を表すテキストNode含む全ての子Node）
+
+テキストNodeを無視して要素Nodeだけを取得したいとき
+→ul.children
+
+最初と最後の子Nodeを取得したいとき（上の場合だと最初と最後の改行部分のテキストNode）
+→ul.firstChild
+→ul.lastChild
+
+最初と最後の要素Nodeだけを取得したいとき
+→firstElementChild
+→lastElementChild
+
+li要素から見て親Nodeを取得したいとき
+→li.parentNode
+
+兄弟Nodeを取得したいとき 
+一つ前の兄弟Node → previousSibling
+一つ後の兄弟Node → nextSibling
+要素だけの場合 (上の場合 liから見るとひとつ前/後のli要素)
+→previousElementSibling
+→nextElementSibling
+
+
+
+
+
+
+
 // 空のリスト、ボタンを配置
 // ボタンをクリックすると、
 // リストの項目が追加される。
